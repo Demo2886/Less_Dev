fb8dbcbbf8840cb2b7ab0a182cc13d90131c161e
Alexandr Nefedin <Demo2886@yandex.by>
2021-05-01 12:01:25 +0300

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "saw_instance" "my-dev" {
+  ami           = "ami-0767046d1677be5a0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Dev-Test"
+  }
+}
+
+resource "aws_instance" "my-hut" {
+  ami           = "ami-009b16df9fcaac611"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "RedHut"
+  }
+}
+
+#comentari
